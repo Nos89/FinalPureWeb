3be5e8b7cfd5969fbf02b8b25953ed804527bcfd
youngju <youngju@email.com>
2021-01-25 17:54:25 +0900

    종합정보페이지 수강과목 시간표 프론트 등

--- src/main/java/kh/spring/controller/HomeController.java
+++ src/main/java/kh/spring/controller/HomeController.java
@@ -5,16 +5,12 @@ import java.util.Locale;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		return "/info/login";
-	}
-	
+
 	@RequestMapping("/main")
 	public String simplePageGoTo(String pageGroup, String type, Model model) {
 		model.addAttribute("type", type);
