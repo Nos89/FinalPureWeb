b51c4857f0df602418aa2d184843fb6270bcb35c
nos <alukard666@naver.com>
2021-01-29 17:10:49 +0900

    학생/교수 구분

--- src/main/java/kh/spring/controller/HomeController.java
+++ src/main/java/kh/spring/controller/HomeController.java
@@ -2,15 +2,23 @@ package kh.spring.controller;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nexacro.uiadapter17.spring.core.data.NexacroResult;
 
 
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	HttpSession session;
+	
 	@RequestMapping("/")
 	public String home(Locale locale, Model model) {
 		return "home";
@@ -35,5 +43,18 @@ public class HomeController {
 	@RequestMapping("/home.nex")
 	public String homeNEX(Locale locale, Model model) {
 		return "redirect:/nex/index.html";
+	}
+	
+	@RequestMapping("/whichUser.nex")
+	public NexacroResult whichUser() {
+		NexacroResult nr = new NexacroResult();
+		String loginID = (String)session.getAttribute("loginID");
+		if( loginID.split("-")[0].contentEquals("S")) {
+			nr.addVariable("userType", "학생");
+		} else if ( loginID.split("-")[0].contentEquals("P")) {
+			nr.addVariable("userType", "교수");
+		}
+		nr.addVariable("loginID", (String)session.getAttribute("loginID"));
+		return nr;
 	}
 }
