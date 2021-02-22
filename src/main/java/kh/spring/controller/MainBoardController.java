06748c48b498f020e85db9889f497cabeffacf5d
PJH <PJH@test.com>
2021-02-22 11:05:30 +0900

    파일다운

--- src/main/java/kh/spring/controller/MainBoardController.java
+++ src/main/java/kh/spring/controller/MainBoardController.java
@@ -1,8 +1,12 @@
 package kh.spring.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +15,7 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FileUtils;
@@ -18,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -276,5 +282,67 @@ public class MainBoardController {
 			currentPage = 1;
 		}
 		return currentPage;
+	}
+	
+	
+	//파일다운로드
+	@RequestMapping("/board.download")
+	public void fileDownlaod(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		String fileName = (String)request.getParameter("fileName");
+		String oriName = (String)request.getParameter("oriName");
+		String contextRoot = new HttpServletRequestWrapper(request).getRealPath("/");
+		String fileRoot = contextRoot + "resources/files/board/";
+		
+		String fullPath = fileRoot + "/" + fileName;
+		File file = new File(fullPath);
+		
+        if(file != null) {
+            fileName = null;
+            String userAgent = request.getHeader("User-Agent");
+            
+            if(userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1){
+                fileName = URLEncoder.encode(file.getName(), "utf-8").replaceAll("\\+", "%20");;
+            }else if(userAgent.indexOf("Chrome") > -1) {
+            	StringBuffer sb = new StringBuffer();
+            	for(int i=0; i<file.getName().length(); i++) {
+            		char c = file.getName().charAt(i);
+            		if(c > '~') {
+            			sb.append(URLEncoder.encode(""+c, "UTF-8"));
+            		}else {
+            			sb.append(c);
+            		}
+            	}
+            	fileName = sb.toString();
+            }else {
+            	fileName = new String(file.getName().getBytes("utf-8"));
+            }
+            response.setContentType(request.getContentType());
+            response.setContentLength((int)file.length());
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + oriName + "\";");
+            response.setHeader("Content-Transfer-Encoding", "binary");
+            
+            OutputStream out = response.getOutputStream();
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+                FileCopyUtils.copy(fis, out);
+            } catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                if(fis != null){
+                    try{
+                        fis.close();
+                    }catch(Exception e){
+                    	e.printStackTrace();
+                    }
+                }
+                
+                if(out != null) {
+                	out.flush();
+                }
+            }
+            
+        }
+		
 	}
 }
